d97c763fabb0a759bc2255e1cce2134135dfc8b7
league <league@imac-18.attlocal.net>
2019-06-29 11:25:22 -0700

    finished backwordsString and started mixed caps

--- src/_00_Text_Funkifier/BackwardsString.java
+++ src/_00_Text_Funkifier/BackwardsString.java
@@ -4,6 +4,15 @@ public class BackwardsString extends SpecialString {
 
 	public BackwardsString(String s) {
 		super(s);
+		
+		String newString = "";
+		for (int j = s.length()-1; j >=0; j--) {
+		
+		newString +=s.charAt(j);
+	
+		
+				}
+		this.s=newString;
 	}
 
 	@Override
--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -1,15 +1,31 @@
 package _00_Text_Funkifier;
-
-public class MixedCapsString extends SpecialString{
+public class MixedCapsString extends SpecialString {
 
 	public MixedCapsString(String s) {
 		super(s);
+String newString = "";
+		
+		
+		for (int i = 0; i < newString.length() - 1; i++) {
+			System.out.println(newString);
+			if (newString.charAt(i) == 'h' || newString.charAt(i) == 's' || newString.charAt(i) == 'i'
+					|| newString.charAt(i) == 'l' || newString.charAt(i) == 'o' || newString.charAt(i) == 'a'
+					|| newString.charAt(i) == 't') {
+
+				newString+= Character.toUpperCase(s.charAt(i));
+			}
+			else {
+				newString+= s.charAt(i);
+			}
+
+		}
+		this.s = newString;
+		System.out.println(s);
 	}
 
 	@Override
 	public String funkifyText(String s) {
-		
+
 		return "";
 	}
-
 }
